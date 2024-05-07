ea0a80f44fec91e14d0e8a047876997da2faa2a3
Kanhaiya <Kanhaiya.dubey@bacancy.com>
2024-05-07 12:25:09 +0530

    React initial commit with functional component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  Functional from './Functional';
+
+function App() {
+  return (
+    <div className="App">
+      <Functional />
+    </div>
+  );
+}
+
+export default App;
